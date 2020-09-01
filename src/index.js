d6bd04f4a6ea74464ba25bcec6e379cfb99d945e
Andrew Hodge <ahodge@tableau.com>
2020-09-01 10:07:06 -0700

    Update path for third_party

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require("js-dos");
 const canvas = document.querySelector("#gameBox");
 
 Dos(canvas, {wdosboxUrl: "../dosbox/wdosbox.js"}).ready((fs, main) => {
-    fs.extract("../games/doom-box.zip").then(() => {
+    fs.extract("../third_party/doom-box.zip").then(() => {
         main(["-c", "DOOM"])
     });
 });
